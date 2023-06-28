ab39da9d83937a4eec493d637044d1f7be1739c1
JM Bae <jungman82@gmail.com>
2023-06-28 09:18:38 +0900

    views.py disqus 설정 추가

--- mysite/settings.py
+++ mysite/settings.py
@@ -132,3 +132,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+DISQUS_SHORTNAME = 'django-wpg5vrrtef'
+DISQUS_MY_DOMAIN = 'http://127.0.0.1:8000'
