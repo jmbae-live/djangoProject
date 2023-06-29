d102a84f71cb329e5b2e50709d9995a67661e4fe
JM Bae <jungman82@gmail.com>
2023-06-29 10:44:49 +0900

    auth 인증 뷰로직, url 추가

--- mysite/urls.py
+++ mysite/urls.py
@@ -24,6 +24,11 @@ from mysite.views import HomeView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    #인증
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/register/', UserCreateView.as_view(), name='register'),
+    path('accounts/register/done/', UserCreateDoneTV.as_view(), name='register_done'),
+
     path('', HomeView.as_view(), name='home'),
     path('blog/', include('blog.urls')),
     path('photo/', include('photo.urls')),
