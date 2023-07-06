90b14c76c8493197f61b9a8551af93326677eee3
Jungman Bae <jmbae@coderails.io>
2023-07-06 10:18:39 +0900

    views.py Album 뷰로직 (작업중)

--- photo/views.py
+++ photo/views.py
@@ -44,3 +44,16 @@ class PhotoDelV(OwnerOnlyMixin, DeleteView):
     success_url = reverse_lazy('photo:index')
 
 
+class AlbumChangeLV(LoginRequiredMixin, ListView):
+    model = Album
+    template_name = 'photo/album_change_list.html'
+
+    def get_queryset(self):
+        return Album.objects.filter(owner=self.request.user)
+
+
+class AlbumDelV(OwnerOnlyMixin, DeleteView):
+    model = Album
+    success_url = reverse_lazy('photo:index')
+
+
